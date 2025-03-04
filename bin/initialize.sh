e48e4414a9bc252944cdca47c87d7774ee84945d
kfujii <fujii.kotaro.l7@elms.hokudai.ac.jp>
2025-03-04 23:12:29 +0000

    Fix: recursive link

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -1,12 +1,21 @@
 #!/bin/zsh
 
 dotfiles_prefix=$HOME/.dotfiles
-ln -s $dotfiles_prefix/zsh/zshrc $HOME/.zshrc
-rm $HOME/.zsh
-ln -s $dotfiles_prefix/zsh $HOME/.zsh
 
+# zsh initialize
+ln -sf $dotfiles_prefix/zsh/zshrc $HOME/.zshrc
+if [[ $(readlink $HOME/.zsh) != $dotfiles_prefix/zsh ]]; then
+    ln -sf $dotfiles_prefix/zsh $HOME/.zsh
+fi
+
+# .config/nvim setting
+if [ ! -d $HOME/.config ]; then
+    mkdir $HOME/.config
+fi
+if [[ $(readlink $HOME/.config/nvim) != $dotfiles_prefix/nvim ]]; then
+    ln -sf $dotfiles_prefix/nvim $HOME/.config/nvim
+fi
+
+# other settings
 ln -sf $dotfiles_prefix/gitconfig $HOME/.gitconfig
 ln -sf $dotfiles_prefix/tmux.conf $HOME/.tmux.conf
-
-mkdir $HOME/.config
-ln -sf $dotfiles_prefix/nvim $HOME/.config/nvim
