9f8d7b0e0cd413b5adeea06a801d8d535098c4b2
kfujii <fujii.kotaro.l7@elms.hokudai.ac.jp>
2025-05-10 12:36:07 +0900

    Change and add some remaps

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act     = wezterm.action
 local config = {}
 config = wezterm.config_builder()
 
@@ -11,6 +12,15 @@ config.font_size = 12.75
 config.color_scheme = 'nightfox'
 config.audible_bell = "Disabled"
 config.hide_tab_bar_if_only_one_tab = true
+
+-- key binds
+config.disable_default_key_bindings = true
+config.keys = {
+    { key = 'v',   mods = 'CTRL|SHIFT', action = act.PasteFrom 'Clipboard' },
+    { key = 'Tab', mods = 'CTRL',       action = act.MoveTabRelative(1) },
+    { key = 'Tab', mods = 'CTRL|SHIFT', action = act.MoveTabRelative(-1) },
+    { key = 't',   mods = 'CTRL|SHIFT', action = act.SpawnTab 'DefaultDomain' },
+}
 
 -- load local settings
 local home_path = os.getenv("USERPROFILE")
