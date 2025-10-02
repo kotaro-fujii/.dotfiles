d0e9d9fdc5b3e93321132960d6d84f0414f230aa
kfujii <fujii.kotaro.l7@elms.hokudai.ac.jp>
2025-10-02 09:02:40 +0900

    Add remap to toggle diagnostics

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,35 @@
+-- ========== functions ==========
+local function toggle_diagnostics()
+  local config = vim.diagnostic.config()
+
+  if config.virtual_text == false then
+    vim.diagnostic.config({
+      virtual_text = {
+        prefix = "+",
+        spacing = 8,
+      },
+      virtual_lines = false,
+    })
+  elseif config.virtual_lines == false then
+    vim.diagnostic.config({
+      virtual_text = false,
+      virtual_lines = {
+        only_current_line = false,
+      },
+    })
+  else
+    vim.diagnostic.config({
+      virtual_text = false,
+      virtual_lines = false,
+    })
+  end
+end
+
 -- ========== settings ==========
 remap_options = { noremap = true, silent = true, buffer = bufnr }
 vim.keymap.set('n', 'K', vim.lsp.buf.definition, remap_options)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, remap_options)
+vim.keymap.set('n', '<C-t>', toggle_diagnostics, remap_options)
 
 vim.diagnostic.config({
     virtual_text = false,
